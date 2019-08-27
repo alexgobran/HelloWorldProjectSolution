affbfd716bf5c24c13b07d03f32ed170764ada9c
alexgobran <54282581+alexgobran@users.noreply.github.com>
2019-08-27 09:26:24 -0400

    Add project files.

---
+++ HelloWorldProject/Program.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+
+using System;
+
+namespace HelloWorldProject {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello C# boot camp World!");
+            Console.WriteLine("At MAX Technical Training");
+            int daysInBootcamp = 66;
+            if (daysInBootcamp == 6)
+            {
+                Console.WriteLine("Yes");
+            }
+            else { Console.WriteLine("No"); }
+
+            
+        string instructor = "";
+        instructor = "Greg";
+            Console.WriteLine(instructor);
+            string lastname = "Doud";
+            //how to concat( combine 2 strings)(+ only can be used to combine strings)
+            Console.WriteLine(instructor + "" + lastname);
+        }
+    }
+}
